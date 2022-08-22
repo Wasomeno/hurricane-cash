7d0a430ecd7452c96dab13b2f2dd565363fdfd8b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-22 14:21:35 +0700

    Basic UI additions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container-fluid vh-100">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
