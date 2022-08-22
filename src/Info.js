8f193b94df24dd7db1a1b618b7132d05f1ac40c7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-22 14:21:04 +0700

    Component for deposit statistics

---
+++ src/Info.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center border border-bottom-0 border-2 border-dark">
+        <h4 className="p-2 m-0">Statistics</h4>
+      </div>
+      <div className="row justify-content-center border border-2 border-dark">
+        <div>
+          <h5 className="text-start p-2 mb-3">Latest Deposits</h5>
+        </div>
+        <div className="row justify-content-center align-items-center">
+          <div className="col">
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+          </div>
+          <div className="col">
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+            <h6 className="text-start bg-dark bg-opacity-25 p-2">
+              5323 7 minutes ago
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
