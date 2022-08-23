dfdd80ddd4865a848caad01de4ad31666ffc26ae
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-23 23:51:31 +0700

    Added get phrases details function

--- src/Info.js
+++ src/Info.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import HurricaneCash from "./abi/HurricaneCash.json";
+import { ethers } from "ethers";
+
+const HurricaneContractAddresss = "0x9193e15224AA6d0d33dA4e23b6534056A2b94561";
 
 const Info = () => {
+  const [phrases, setPhrases] = useState([]);
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const hurricaneContract = new ethers.Contract(
+    HurricaneContractAddresss,
+    HurricaneCash.abi,
+    signer
+  );
+
+  async function getPhrases() {
+    await hurricaneContract.getPhrasesDetails().then((response) => {
+      setPhrases(response);
+      console.log(response);
+    });
+  }
+
+  useEffect(() => {
+    getPhrases();
+  }, []);
+
   return (
     <div className="container-fluid">
       <div className="row justify-content-center border border-bottom-0 border-2 border-dark">
@@ -10,34 +34,18 @@ const Info = () => {
         <div>
           <h5 className="text-start p-2 mb-3">Latest Deposits</h5>
         </div>
-        <div className="row justify-content-center align-items-center">
-          <div className="col">
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-          </div>
-          <div className="col">
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
-            <h6 className="text-start bg-dark bg-opacity-25 p-2">
-              5323 7 minutes ago
-            </h6>
+        <div className="row justify-content-start align-items-center">
+          <div className="col-6">
+            {phrases.map((phrase, index) => (
+              <h6 key={index} className="text-start bg-dark bg-opacity-25 p-2">
+                {"tx" + (index + 1)}{" "}
+                {(
+                  (Date.now().toString() / 1000 - phrase.timeStamp.toString()) /
+                  3600
+                ).toFixed(1)}{" "}
+                hours ago
+              </h6>
+            ))}
           </div>
         </div>
       </div>
