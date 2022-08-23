2bc479e194899b29d03c401db68ea8143b8e2b3b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-23 23:31:58 +0700

    Added connect wallet function

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ account, setAccount }) => {
+  const isConnected = Boolean(account[0]);
+  async function connectAccount() {
+    if (window.ethereum) {
+      const account = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(account);
+    }
+  }
   return (
     <>
       <div className="row justify-content-between align-items-center p-3">
@@ -10,7 +19,18 @@ const Navbar = () => {
           <h5 className="m-0 mx-3">Hurricane.cash</h5>
         </div>
         <div className="col-3">
-          <button className="btn btn-primary">Connect To Wallet</button>
+          {isConnected ? (
+            <button className="border border-2 border-dark p-3 ">
+              {account[0].slice(0, 7)}....{account[0].slice(35, 42)}
+            </button>
+          ) : (
+            <button
+              className="btn btn-dark text-white p-3"
+              onClick={connectAccount}
+            >
+              Connect To Wallet
+            </button>
+          )}
         </div>
       </div>
       <Outlet />
