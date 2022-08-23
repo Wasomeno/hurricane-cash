ffdce384135b875de9dfe0082096300601928efe
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-23 23:32:44 +0700

    Added not connected component

---
+++ src/NotConnected.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotConnected = () => {
+  return (
+    <div className="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+      <h3 className="fw-bold">Connect your wallet first</h3>
+    </div>
+  );
+};
+
+export default NotConnected;
